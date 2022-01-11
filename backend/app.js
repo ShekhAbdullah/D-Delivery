1f79bb6e2e55a3eb4980c616b7361b330f69e145
ShekhAbdullah <76776291+ShekhAbdullah@users.noreply.github.com>
2022-01-12 00:53:22 +0600

    D-Delivery Final Defense Project has Been Started

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+//Route Imports
+const product = require('./routes/productRoute');
+
+app.use("/api/v1",product);
+
+
+
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const app = require('./app');
+
+const dotenv = require('dotenv');
+const connectDatabase = require("./config/database")
+
+
+
+
+//config 
+
+dotenv.config({path:"backend/config/config.env"})
+
+//connecting to connectDatabase
+  
+ connectDatabase()
+ 
+
+
+app.listen(process.env.PORT, ()=>{
+
+
+    console.log(`server is working on http://localhost:${process.env.PORT}`);
+})
